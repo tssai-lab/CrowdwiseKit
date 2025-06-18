8cda969634746eacf4d31b8cc4fe7b64ad4fe681
stop-hand <126220327+stop-hand@users.noreply.github.com>
2025-06-18 10:58:01 +0800

    Delete src/Learn/MFDFAGen-Net directory

--- src/Learn/MFDFAGen-Net/my_work/loss.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-#预定义的损失函数
-loss_fn = torch.nn.CrossEntropyLoss(reduction='mean').cuda()
-def multi_loss(y_true, y_pred, loss_fn=torch.nn.CrossEntropyLoss(reduce='mean').cuda()):
-    mask = y_true != -1
-    y_pred = torch.transpose(y_pred, 1, 2)
-    loss = loss_fn(y_pred[mask], y_true[mask])
-    return loss
